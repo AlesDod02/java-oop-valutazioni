e70a0815bc072108cf8f971c6ae756a7fdf8fd35
AlesDod02 <alessandro.dod@gmail.com>
2023-12-13 15:48:24 +0100

    working

--- src/org/lessons/java/valutazioni/Corso.java
+++ src/org/lessons/java/valutazioni/Corso.java
@@ -1,2 +1,44 @@
-package org.lessons.java.valutazioni;public class Corso {
+package org.lessons.java.valutazioni;
+
+import java.util.ArrayList;
+
+
+public class Corso {
+    private static ArrayList<Studente>gianni= new ArrayList<>();
+
+
+    public static void getStudents(ArrayList<Studente>gianni){
+        for (Studente studente: gianni){
+            System.out.println(studente);
+        }
+
+
+    }
+    public static void removeStudent(ArrayList<Studente>gianni,int ID){
+        gianni.remove(ID);
+
+    }
+    public static void addStudent(ArrayList<Studente>gianni,Studente nuovoStudente){
+        gianni.remove(nuovoStudente);
+
+    }
+    public static  void getPercStudentiPromossi (ArrayList<Studente>gianni){
+        ArrayList<Studente>promossi= new ArrayList<>();
+        for (Studente studente:gianni){
+            if (studente.getStudentResult()){
+                promossi.add(studente);
+
+            }
+
+
+        }
+        System.out.println("la percentuale di studenti promossi è:"+(promossi.size()*100.0/(promossi.size()+gianni.size())));
+
+    }
+
+
+
+
+
+
 }
